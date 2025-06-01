cca0a70a946efe2cb9447033092509bf665b6218
Mikail <mikailsyed@yahoo.com>
2025-06-01 19:11:47 -0400

    return da pointer

--- graph.c
+++ graph.c
@@ -2,11 +2,11 @@
 #include <graph.h>
 
 static inline node_t* get_nbr_node(interface_t *interface) {
-    return &interface->attrNode;
+    return interface->attrNode;
 }
 
 static inline int get_node_intf_available_slot(node_t *node) {
-    for(int i=0; i < MAX_INTERFACES_PER_NODE; i++) {
+    for(int i=0; i < MAX_INTF_PER_NODE; i++) {
         if(node->interfaces[i] == NULL)
             return i;
     }
