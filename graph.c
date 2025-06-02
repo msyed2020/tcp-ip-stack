0d900f90e96efa5594d6172c4a0bd9e684b66fec
Mikail <mikailsyed@yahoo.com>
2025-06-02 11:25:03 -0400

    create graph node

--- graph.c
+++ graph.c
@@ -22,3 +22,12 @@ graph_t *createNewGraph(char *topologyName) {
     init_glthread(&graph->nodeList);
     return graph;
 }
+
+node_t *createGraphNode(graph_t *graph, char *nodeName) {
+    node_t *node = calloc(1, sizeof(node_t));
+    strncpy(node->nodeName, nodeName, NODE_NAME_SIZE);
+    node->nodeName[NODE_NAME_SIZE] = '\0';
+    init_glthread(&node->graphGlue);
+    glthreadAddNext(&graph->nodeList, &node->graphGlue);
+    return node;
+}
