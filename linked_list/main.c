dead5c86765358b5bae79fa5941948a7b2b098b7
Mikail <mikailsyed@yahoo.com>
2025-06-04 12:12:16 -0400

    add test driver

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,7 +1,53 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "linked_list.h"
 
-int main(int argc, char argv[]) {
-    
+typedef struct my_node {
+    int data;
+    glued_ll_node_t glued_node;
+} my_node_t;
+
+int main() {
+    glued_ll_t list;
+    initGluedLL(&list, offsetof(my_node_t, glued_node));
+
+    // Create and add nodes
+    for (int i = 0; i < 5; i++) {
+        my_node_t *new_node = (my_node_t *)malloc(sizeof(my_node_t));
+        new_node->data = i;
+        gluedLLNodeInit(&new_node->glued_node);
+        gluedLLAddFront(&list, &new_node->glued_node);
+        printf("Added node with data: %d\n", i);
+    }
+
+    // Iterate and print nodes
+    printf("\nList contents:\n");
+    my_node_t *node_ptr;
+    ITERATE_GLUED_LL_BEGIN(&list, my_node_t, node_ptr)
+        printf("Node data: %d\n", node_ptr->data);
+    ITERATE_GLUED_LL_END
+
+    // Remove the head node
+    if (list.head) {
+        node_ptr = (my_node_t *)((char *)list.head - list.offset);
+        printf("\nRemoving node with data: %d\n", node_ptr->data);
+        gluedLLRemoveNode(&list, &node_ptr->glued_node);
+        free(node_ptr);
+    }
+
+    // Print list after removal
+    printf("\nList contents after removal:\n");
+    ITERATE_GLUED_LL_BEGIN(&list, my_node_t, node_ptr)
+        printf("Node data: %d\n", node_ptr->data);
+    ITERATE_GLUED_LL_END
+
+    // Free remaining nodes
+    ITERATE_GLUED_LL_BEGIN(&list, my_node_t, node_ptr)
+        glued_ll_node_t *temp = &node_ptr->glued_node;
+        gluedLLRemoveNode(&list, temp);
+        free(node_ptr);
+    ITERATE_GLUED_LL_END
+
+    return 0;
 }
