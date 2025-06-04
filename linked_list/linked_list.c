9b1734c4ffc15955cd6c29d7e796a15b177064f9
Mikail <mikailsyed@yahoo.com>
2025-06-03 20:36:37 -0400

    more

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void gluedLLAddNextNode(glued_ll_node_t *curr, glued_ll_node_t *next) {
+static void gluedLLAddNextNode(glued_ll_node_t *curr, glued_ll_node_t *newNode) {
     if (!curr->right) {
-        
+        curr->right = newNode;
+        newNode->left = curr;
+        return;
     }
+
+    
 }
