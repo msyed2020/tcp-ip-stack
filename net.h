ff806bac6986ea2aebaf4d09f7d44c363cf1e3be
Mikail <mikailsyed@yahoo.com>
2025-06-07 19:54:54 -0400

    comments

--- net.h
+++ net.h
@@ -3,9 +3,13 @@
 #ifndef __NET__
 #define __NET__
 
+// Every node has its own IP address, called as the loopback address
+
 typedef struct ip_add { // IP address
     char ip_addr[16];
 } ip_add_t;
+
+// Every interace MUST have a MAC address and could have an IP address or mask
 
 typedef struct mac_add { // MAC address
     char mac[48];
