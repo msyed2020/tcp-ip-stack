fd41ee2987dbdf0367bb87ec0d03fffee9df7e5d
Mikail <mikailsyed@yahoo.com>
2025-06-08 12:03:41 -0400

    first func

--- net.c
+++ net.c
@@ -1,1 +1,11 @@
 #include "net.h"
+
+bool_t nodeSetLoopbackAddress(node_t *node, char *ip_addr) {
+    assert(ip_addr);
+
+    node->nodeNetworkProp.isLoopback = true;
+    strncpy(NODE_LO_ADDR(node), ip_addr, 16);
+    NODE_LO_ADDR(node)[15] = '\0';
+    return true;
+}
+
