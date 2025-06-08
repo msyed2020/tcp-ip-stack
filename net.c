e94e00ec698e0ca26bb543b2a21d9dab53985e95
Mikail <mikailsyed@yahoo.com>
2025-06-08 12:08:52 -0400

    add next api

--- net.c
+++ net.c
@@ -1,4 +1,5 @@
 #include "net.h"
+#include "graph.c"
 
 bool_t nodeSetLoopbackAddress(node_t *node, char *ip_addr) {
     assert(ip_addr);
@@ -9,3 +10,18 @@ bool_t nodeSetLoopbackAddress(node_t *node, char *ip_addr) {
     return true;
 }
 
+bool_t nodeSetInterfaceIPAddress(node_t *node, char *localInterface, char *ip_addr, char mask) {
+    interface_t *interface = getNodeIfByName(node, localInterface);
+    if (!interface) {
+        assert(0);
+    }
+    strncpy(IF_IP(interface), ip_addr, 16);
+    IF_IP(interface)[15] = '\0';
+    interface->interfaceNetworkProp.mask = mask;
+    interface->interfaceNetworkProp.isIP = true;
+    return true;
+}
+
+bool_t nodeRemoveInterfaceIPAddress(node_t *node, char *localInterface) {
+    return true;
+}
